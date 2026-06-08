82368d4ae59a557e360f1a6ed6d468d978385f63
jockerf6 <fhakem75@gmail.com>
2026-06-09 02:18:38 +0300

    chore: remove unused customer module and associated controllers, services, and DTOs

--- src/_modules/user/user.module.ts
+++ src/_modules/user/user.module.ts
@@ -6,14 +6,11 @@ import { MeController } from './controllers/me.controller';
 import { UserController } from './controllers/user.controller';
 import { HelperService } from './services/helper.service';
 import { UserService } from './services/user.service';
-import { CustomerModule } from './_modules/customer/customer.module';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [],
   controllers: [MeController, UserController],
   providers: [UserService, OTPService, TokenService, HelperService],
   exports: [UserService],
 })
 export class UserModule {}
-
-
